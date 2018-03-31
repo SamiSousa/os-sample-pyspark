86383bc885b7e7b46be1cb7be3d9a46fc2e0de26
SamiSousa <ssshahin@bu.edu>
2018-03-31 15:29:57 -0400

    Adding main.py and data.txt

---
+++ main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# Reference: https://stackoverflow.com/a/32845282
+
+from __future__ import print_function
+import re, sys
+from pyspark import SparkContext, SparkConf
+
+def linesToWordsFunc(line):
+    wordsList = line.split()
+    wordsList = [re.sub(r'\W+', '', word) for word in wordsList]
+    filtered = filter(lambda word: re.match(r'\w+', word), wordsList)
+    return filtered
+
+def wordsToPairsFunc(word):
+    return (word, 1)
+
+def reduceToCount(a, b):
+    return (a + b)
+
+def main():
+    conf = SparkConf().setAppName("Words count").setMaster("local")
+    sc = SparkContext(conf=conf)
+    rdd = sc.textFile("data.txt")
+
+    words = rdd.flatMap(linesToWordsFunc)
+    pairs = words.map(wordsToPairsFunc)
+    counts = pairs.reduceByKey(reduceToCount)
+
+    # Get the first top 100 words
+    output = counts.takeOrdered(100, lambda (k, v): -v)
+
+    for(word, count) in output:
+        # print to stderr so we can see it in the pod's logs (maybe?)
+        print( word, ':', str(count), file=sys.stderr)
+
+    sc.stop()
+
+if __name__ == "__main__":
+    main()
